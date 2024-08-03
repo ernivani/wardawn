2a4fb19c24212ee5a412b4bded2a98441a3ad5a6
Thomas Lindeker <tlindeker0@gmail.com>
2024-08-04 00:15:32 +0200

    Added maxHealth to Player prefab, refactored PlayerMovementController to inherit Entity, and updated PlayerStamina

---
+++ Assets/Scripts/Abstract/Entity.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+{
+    [Header("Entity Stats")]
+    public float maxHealth = 100f;
+
+    protected float currentHealth;
+
+    protected virtual void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public abstract void TakeDamage(float amount);
+
+    public virtual void Heal(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    public bool IsAlive()
+    {
+        return currentHealth > 0;
+    }
+}
